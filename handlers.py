d60c40c1a7ad71114fceb5d97ce046d239fffbe8
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2020-06-19 07:19:58 +0530

    Minor renaming, everything added: including aggregator. Testing remains.

--- handlers.py
+++ handlers.py
@@ -7,18 +7,18 @@ from config.constants import ReactionTypes, RowKeys
 from db_layer import get_items_from_table, update_item_in_table
 
 
-def _update_window(req_id, window_reactions, curr_tick_reactions, operation_timestamp):
+def _update_window(req_id, window_reactions_doc, curr_tick_reactions_doc, operation_timestamp):
     update_expressions = []
     values_for_update_expression = {
         ":lastUpdatedTimestamp": operation_timestamp
     }
 
     for reaction_type in ReactionTypes:
-        window_reactions[reaction_type.value].append(curr_tick_reactions[reaction_type.value])
-        if len(window_reactions[reaction_type.value]) > 0:
-            window_reactions[reaction_type.value].pop(0)
+        window_reactions_doc[reaction_type.value].append(curr_tick_reactions_doc[reaction_type.value])
+        if len(window_reactions_doc[reaction_type.value]) > 0:
+            window_reactions_doc[reaction_type.value].pop(0)
         update_expressions.append("{r} = :{r}".format(r=reaction_type.value))
-        values_for_update_expression[reaction_type.value] = window_reactions[reaction_type.value]
+        values_for_update_expression[reaction_type.value] = window_reactions_doc[reaction_type.value]
 
     update_expression = "SET {}, lastUpdatedTimestamp = :lastUpdatedTimestamp".format(", ".join(update_expressions))
 
@@ -34,7 +34,8 @@ def _update_reactions_for_curr_tick_in_table(req_id, reactions, op_type):
     update_expression = "SET {}".format(", ".join(update_expressions))
 
     values_for_update_expression = dict(reduce(lambda acc, reactionType:
-                                               acc + [(":{}".format(reactionType.value), reactions[reactionType.value])],
+                                               acc + [
+                                                   (":{}".format(reactionType.value), reactions[reactionType.value])],
                                                ReactionTypes, []))
 
     update_item_in_table(req_id, get_key_value_for_primary_key(RowKeys.CURR_TICK.value), update_expression,
@@ -51,29 +52,38 @@ def _mark_window_as_in_progress(req_id):
                          values_for_update_expression, condition_to_check)
 
 
-def _try_updating_window_and_resetting_tick(req_id, window_reactions, curr_tick_reactions, operation_timestamp):
+def _try_updating_window_and_resetting_tick(req_id, window_reactions_doc, curr_tick_reactions_doc, operation_timestamp):
     try:
         _mark_window_as_in_progress(req_id)
     except ClientError as err:
         log_line(req_id, "Window Updation already in Progress")
     else:
-        _update_window(req_id, window_reactions, curr_tick_reactions, operation_timestamp)
-        _update_reactions_for_curr_tick_in_table(req_id, curr_tick_reactions, "-")
+        _update_window(req_id, window_reactions_doc, curr_tick_reactions_doc, operation_timestamp)
+        _update_reactions_for_curr_tick_in_table(req_id, curr_tick_reactions_doc, "-")
 
 
-def _sum_up_reactions(window_reactions, curr_tick_reactions):
-    return True
+def _aggregate_reaction(acc, reaction_type, window_reaction, curr_tick_reaction):
+    acc[reaction_type] = reduce(lambda x,y: x+y, window_reaction) + curr_tick_reaction
+    return acc
+
+
+def _sum_up_reactions(window_reactions_doc, curr_tick_reactions_doc):
+    return reduce(lambda acc, reaction_type: _aggregate_reaction(acc, reaction_type.value,
+                                                                 window_reactions_doc[reaction_type.value],
+                                                                 curr_tick_reactions_doc[reaction_type.value]),
+                  ReactionTypes, {})
 
 
 def get_reactions(req_id):
     serialised_window_reactions, serialised_curr_tick_reactions = get_items_from_table(req_id)
     curr_timestamp = get_timestamp()
-    window_reactions = deserialize_db_objects(serialised_window_reactions)
-    curr_tick_reactions = deserialize_db_objects(serialised_curr_tick_reactions)
-    aggregated_reactions = _sum_up_reactions(window_reactions, curr_tick_reactions)
+    window_reactions_doc = deserialize_db_objects(serialised_window_reactions)
+    curr_tick_reactions_doc = deserialize_db_objects(serialised_curr_tick_reactions)
+    aggregated_reactions = _sum_up_reactions(window_reactions_doc, curr_tick_reactions_doc)
 
-    if not window_reactions["isCurrentlyUpdating"] or curr_timestamp - window_reactions['lastUpdatedTimestamp'] > 1000:
-        _try_updating_window_and_resetting_tick(window_reactions, curr_tick_reactions, curr_timestamp)
+    if not window_reactions_doc["isCurrentlyUpdating"] or curr_timestamp - window_reactions_doc[
+        'lastUpdatedTimestamp'] > 1000:
+        _try_updating_window_and_resetting_tick(window_reactions_doc, curr_tick_reactions_doc, curr_timestamp)
 
     return aggregated_reactions
 
